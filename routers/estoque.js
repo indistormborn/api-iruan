cb5e44d41bfbb9c4aab197504644a33e241b06fd
Indi <indicm@gmail.com>
2025-07-03 23:14:30 -0300

    alterando quantidade do produto quando registra entrada ou saida

--- routers/estoque.js
+++ routers/estoque.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import { Entrada, Saida } from '../db.js';
+import { Entrada, Produto, Saida } from '../db.js';
 import { getUserFromToken } from '../token.js';
 import logger from '../logger.js';
 
@@ -12,8 +12,20 @@ router.post('/estoque/entrada', async (req, res) => {
       return res.status(401).json({ error: 'Usuário não autenticado' });
     }
     logger.info(`Usuário ${user.username} registrando entrada de estoque`, { entrada: req.body });
-    const novaEntrada = await Entrada.create({...req.body, user: user.id_user });
-    res.status(201).json(novaEntrada);
+    
+    const produto = await Produto.findByPk(req.body.id_produto);
+    if (!produto) {
+      logger.error('Produto não encontrado ao registrar entrada de estoque', { id_produto: req.body.id_produto });
+      return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+
+    produto.quantidade += req.body.quantidade;
+    await produto.save();
+    // Registrar a entrada no histórico
+    logger.info(`Entrada registrada para o produto ${produto.nome}`, { quantidade: req.body.quantidade });
+    await Entrada.create({...req.body, user: user.id_user });
+
+    res.status(201).json(produto);
   } catch (error) {
     logger.error('Erro ao registrar entrada de estoque', { error });
     res.status(500).json({ error: 'Erro ao registrar entrada de estoque' });
@@ -27,9 +39,24 @@ router.post('/estoque/saida', async (req, res) => {
       logger.error('Usuário não autenticado ao registrar saída de estoque');
       return res.status(401).json({ error: 'Usuário não autenticado' });
     }
+
+    const produto = await Produto.findByPk(req.body.id_produto);
+    if (!produto) {
+      logger.error('Produto não encontrado ao registrar saída de estoque', { id_produto: req.body.id_produto });
+      return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+
+    if (produto.quantidade < req.body.quantidade) {
+      logger.warn('Quantidade insuficiente em estoque para registrar saída', { produto: produto.nome, quantidade: req.body.quantidade });
+      return res.status(400).json({ error: 'Quantidade insuficiente em estoque' });
+    }
+
+    produto.quantidade -= req.body.quantidade;
+    await produto.save();
+
     logger.info(`Usuário ${user.username} registrando saída de estoque`, { saida: req.body });
-    const novaSaida = await Saida.create({...req.body, user: user.id_user });
-    res.status(201).json(novaSaida);
+    await Saida.create({...req.body, user: user.id_user });
+    res.status(201).json(produto);
   } catch (error) {
     logger.error('Erro ao registrar saída de estoque', { error });
     res.status(500).json({ error: 'Erro ao registrar saída de estoque' });
