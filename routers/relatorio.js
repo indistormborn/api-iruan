28d8fa0c4dbc8670064be9f4c88a4626a91e4e22
Indi <indicm@gmail.com>
2025-07-04 19:11:37 -0300

    prettier e filtrando data e melhorando data

--- routers/relatorio.js
+++ routers/relatorio.js
@@ -1,71 +1,101 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import { Entrada, Saida } from '../db.js';
-import { Op } from 'sequelize';
-import { getUserFromToken } from '../token.js';
-import logger from '../logger.js';
+import { Entrada, Saida } from "../db.js";
+import { Op } from "sequelize";
+import { getUserFromToken } from "../token.js";
+import logger from "../logger.js";
 
-router.post('/relatorio/estoque', async (req, res) => {
+router.post("/relatorio/estoque", async (req, res) => {
   try {
     const user = getUserFromToken(req, res);
     if (!user) {
-      logger.error('Usuário não autenticado');
-      return res.status(401).json({ error: 'Usuário não autenticado' });
+      logger.error("Usuário não autenticado");
+      return res.status(401).json({ error: "Usuário não autenticado" });
     }
     const body = req.body;
     if (!body || !body.data_inicio || !body.data_fim) {
-      logger.warn('Dados de início ou fim são obrigatórios');
-      return res.status(400).json({ error: 'Dados de início ou fim são obrigatórios' });
+      logger.warn("Dados de início ou fim são obrigatórios");
+      return res
+        .status(400)
+        .json({ error: "Dados de início ou fim são obrigatórios" });
     }
 
-    logger.info(`Usuário ${user.username} solicitando relatório de estoque`, { data_inicio: body.data_inicio, data_fim: body.data_fim });
+    logger.info(`Usuário ${user.username} solicitando relatório de estoque`, {
+      data_inicio: body.data_inicio,
+      data_fim: body.data_fim,
+    });
 
     const dataInicio = body.data_inicio ? new Date(body.data_inicio) : null;
     const dataFim = body.data_fim ? new Date(body.data_fim) : null;
     const query = { user: user.id_user };
 
-    logger.info(`Usuário ${user.username} gerando relatório de estoque`, { query: JSON.stringify(query) });
+    logger.info(`Usuário ${user.username} gerando relatório de estoque`, {
+      query: JSON.stringify(query),
+    });
     const [relatorioEntrada, relatorioSaida] = await Promise.all([
-      Entrada.findAll({ 
+      Entrada.findAll({
         where: query,
-        include: [{ association: 'Produto' }] // Certifique-se de que a associação 'produto' está definida no modelo Entrada
-      }).then(entradas => {
-        return entradas.sort((a, b) => new Date(a.data_hora) - new Date(b.data_hora));
+        include: [{ association: "Produto" }], // Certifique-se de que a associação 'produto' está definida no modelo Entrada
+      }).then((entradas) => {
+        return entradas
+          .filter((entrada) => {
+            const data = new Date(entrada.data_hora);
+            data.setHours(0, 0, 0, 0);
+            return (
+              (!dataInicio || data >= dataInicio) &&
+              (!dataFim || data <= dataFim)
+            );
+          })
+          .sort((a, b) => new Date(a.data_hora) - new Date(b.data_hora));
       }),
       Saida.findAll({
         where: query,
-        include: [{ association: 'Produto' }] // Certifique-se de que a associação 'produto' está definida no modelo Saida
-      }).then(saidas => {
-        return saidas.sort((a, b) => new Date(a.data_hora) - new Date(b.data_hora));
-      })
+        include: [{ association: "Produto" }], // Certifique-se de que a associação 'produto' está definida no modelo Saida
+      }).then((saidas) => {
+        return saidas
+          .filter((saida) => {
+            const data = new Date(saida.data_hora);
+            data.setHours(0, 0, 0, 0);
+
+            return (
+              (!dataInicio || data >= dataInicio) &&
+              (!dataFim || data <= dataFim)
+            );
+          })
+          .sort((a, b) => new Date(a.data_hora) - new Date(b.data_hora));
+      }),
     ]);
 
-    const relatorio = configuraEstruturaDoRelatorio(relatorioEntrada, relatorioSaida);
+    const relatorio = configuraEstruturaDoRelatorio(
+      relatorioEntrada,
+      relatorioSaida
+    );
 
     res.json(relatorio);
   } catch (error) {
-    logger.error('Erro ao gerar relatório de estoque', { error });
-    res.status(500).json({ error: 'Erro ao gerar relatório de estoque' });
+    logger.error("Erro ao gerar relatório de estoque", { error });
+    res.status(500).json({ error: "Erro ao gerar relatório de estoque" });
   }
 });
 
 const configuraEstruturaDoRelatorio = (entradas, saidas) => {
-  const parsedEntradas = entradas.map(entrada => ({
-    tipo: 'Entrada',
+  const parsedEntradas = entradas.map((entrada) => ({
+    tipo: "Entrada",
     quantidade: entrada.quantidade,
     produto: entrada.Produto ? entrada.Produto.nome : entrada.id_produto, // Use o nome do produto se disponível
     data_hora: entrada.data_hora,
   }));
 
-  const parsedSaidas = saidas.map(saida => ({
-    tipo: 'Saída',
+  const parsedSaidas = saidas.map((saida) => ({
+    tipo: "Saída",
     quantidade: saida.quantidade,
     produto: saida.Produto ? saida.Produto.nome : saida.id_produto, // Use o nome do produto se disponível
     data_hora: saida.data_hora,
   }));
 
-  return parsedEntradas.concat(parsedSaidas)
+  return parsedEntradas
+    .concat(parsedSaidas)
     .sort((a, b) => new Date(a.data_hora) - new Date(b.data_hora));
-}
+};
 
 export default router;
